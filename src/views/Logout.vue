135f77f498c847dd3bde54088c167e32566afd76
munnam sateesh <munnamsateesh@gmail.com>
2024-02-17 15:08:51 +0530

    updated file

---
+++ src/views/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="logout">Logout</button>
+</template>
+
+<script>
+import authService from '@/services/authService';
+
+export default {
+  methods: {
+    logout() {
+      authService.logout();
+      // Redirect to the login page upon logout
+      this.$router.push({ name: 'Login' });
+    },
+  },
+};
+</script>
